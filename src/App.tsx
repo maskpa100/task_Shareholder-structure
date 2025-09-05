7c54a33ebca8122fb20d789d14453d53fe1a0332
maskpa100 <maskpa@yandex.ru>
2025-09-05 12:55:36 +0800

    h

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import './App.css'
 import dataJson from "./../public/api/data.json";
 import { StocksTable } from "./components/StocksTable/Table";
 import { PieWithLegend } from "./components/RoundPieChart/RoundPieChart";
